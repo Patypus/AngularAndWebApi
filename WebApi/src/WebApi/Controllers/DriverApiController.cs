7a8ecf149b35e42c670d3fa54c2848743c9e739d
Pat Howell <patrick.howell@outlook.com>
2016-05-11 22:13:13 +0100

    Minor changes without getting stuck into anything

---
+++ WebApi/src/WebApi/Controllers/DriverApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    public class DriverApiController : Controller
+    {
+        public JsonResult GetAllDriverRecords()
+        {
+            return Json("");
+        }
+    }
+}
