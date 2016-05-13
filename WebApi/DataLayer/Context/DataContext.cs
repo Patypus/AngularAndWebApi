a671eaf41ee81233b538f8051824de28d296d687
Pat Howell <patrick.howell@outlook.com>
2016-05-13 22:08:35 +0100

    Starting to setup data layer

---
+++ WebApi/DataLayer/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DataContext")
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
