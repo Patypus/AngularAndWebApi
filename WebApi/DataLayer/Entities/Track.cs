77defc5265a9d7f5f3f5576af5e7550077378949
Pat Howell <patrick.howell@outlook.com>
2016-06-01 22:03:05 +0100

    Started adding more entities

---
+++ WebApi/DataLayer/Entities/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class Track
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int LapLengthMeters { get; set; }
+
+        public virtual ICollection<Race> Races { get; set; }
+    }
+}
